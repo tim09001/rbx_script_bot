732f23181e713a7fdcd42ee5aca7c28b48f9e30f
tim09001 <samat.yamalutdinov@mail.ru>
2026-01-14 18:42:58 +0300

    Delete install_deps.py

--- install_deps.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-import sys
-
-# Устанавливаем зависимости
-subprocess.check_call([sys.executable, "-m", "pip", "install", "telethon", "requests"])
-
-print("Зависимости установлены!")
