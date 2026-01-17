c02313ac77b08afeff354ca63663cb66616d1dce
tim09001 <samat.yamalutdinov@mail.ru>
2026-01-17 13:59:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ logger = logging.getLogger(__name__)
 # Конфигурация
 API_ID = '27231812'
 API_HASH = '59d6d299a99f9bb97fcbf5645d9d91e9'
-BOT_TOKEN = '8502910736:AAEP7FzHxnnIsuYsK6HOlcX317LZvzMKZTM'
+BOT_TOKEN = '8502910736:AAEHCK5bWfKv06oWZaPng-6G9zRcD8tDoBw'
 ADMINS = [262511724]  # ID администраторов
 LOG_CHANNEL = 'https://t.me/+cnym32Oi-mJiMGNi'  # Ссылка на канал логов
 
